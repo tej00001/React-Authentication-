c7c8444250b50d612d588cabaf37f93c9c152ee0
tej00001 <tejachintu207@gmail.com>
2023-02-25 23:40:57 +0530

    Adding Task17 Managing the token for password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 import classes from "./AuthForm.module.css";
+import AuthContext from "../store/auth-context";
 
 const AuthForm = () => {
   const emailInpurRef = useRef();
   const passwordInputRef = useRef();
+
+  const authCtx =  useContext(AuthContext);
+
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
@@ -55,7 +59,7 @@ const AuthForm = () => {
       })
 
       .then((data) => {
-        console.log(data);
+        authCtx.login(data.idToken);
       })
       .catch((err) => {
         alert(err.message);
